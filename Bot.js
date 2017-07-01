61668a0aae6ff0c0dccb5aecf9a100c4cc189cd3
Alameer Ashraf Hassan <alamiir.ashraf@gmail.com>
2017-07-01 02:35:13 +0200

    Bot

--- Bot.js
+++ Bot.js
@@ -33,8 +33,7 @@ client.connect(dbaccessurl, function (err, db) {
         });
 
         app.get('/index', function (req, res) {
-            res.send("1719244614");
-            console.log("as")
+            res.send("hisham 7abib qalbi");
         })
 
         app.get('/Webhock',function(req,res){
@@ -51,14 +50,110 @@ client.connect(dbaccessurl, function (err, db) {
 
 
         // Receive Messages 
-        app.post('/Webhock',function(req,res){
+        app.post('/webhook', function (req, res) {
+            console.log("assss");
             var data = req.body;
-            if (data.object == 'page'){
-                data.entry.forEach(function(entry) {
-                    
+
+            // Make sure this is a page subscription
+            if (data.object === 'page') {
+
+                // Iterate over each entry - there may be multiple if batched
+                data.entry.forEach(function (entry) {
+                    var pageID = entry.id;
+                    var timeOfEvent = entry.time;
+
+                    // Iterate over each messaging event
+                    entry.messaging.forEach(function (event) {
+                        if (event.message) {
+                            receivedMessage(event);
+                        } else {
+                            console.log("Webhook received unknown event: ", event);
+                        }
+                    });
                 });
+
+                // Assume all went well.
+                //
+                // You must send back a 200, within 20 seconds, to let us know
+                // you've successfully received the callback. Otherwise, the request
+                // will time out and we will keep trying to resend.
+                res.sendStatus(200);
             }
-        })
+        });
+
+
+
+        function receivedMessage(event) {
+            var senderID = event.sender.id;
+            var recipientID = event.recipient.id;
+            var timeOfMessage = event.timestamp;
+            var message = event.message;
+
+            console.log("Received message for user %d and page %d at %d with message:",
+                senderID, recipientID, timeOfMessage);
+            console.log(JSON.stringify(message));
+
+            var messageId = message.mid;
+
+            var messageText = message.text;
+            var messageAttachments = message.attachments;
+
+            if (messageText) {
+
+                // If we receive a text message, check to see if it matches a keyword
+                // and send back the example. Otherwise, just echo the text we received.
+                switch (messageText) {
+                    case 'generic':
+                        sendGenericMessage(senderID);
+                        break;
+
+                    default:
+                        sendTextMessage(senderID, messageText);
+                }
+            } else if (messageAttachments) {
+                sendTextMessage(senderID, "Message with attachment received");
+            }
+        }
+
+
+
+
+        function sendTextMessage(recipientId, messageText) {
+            var messageData = {
+                recipient: {
+                    id: recipientId
+                },
+                message: {
+                    text: messageText
+                }
+            };
+
+            callSendAPI(messageData);
+        }
+
+
+
+        function callSendAPI(messageData) {
+            request({
+                uri: 'https://graph.facebook.com/v2.6/me/messages',
+                qs: { access_token: "EAAbgqpQOVhEBAOdI2GhDudnBf09S4foZBvcH92Vd0NkUpf89S0d7vSX5ZChxUqp6LxUFSt5XnrGfS19FDSURjbYIB0LOhQm3cUcxEv2wjVL4NsTs2pBGHry1gQ0Ia1zplFt1byaMKYh9sRtZAHEfkdZC9jpqSLZAwkz3A91lGDAZDZD" },
+                method: 'POST',
+                json: messageData
+
+            }, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var recipientId = body.recipient_id;
+                    var messageId = body.message_id;
+
+                    console.log("Successfully sent generic message with id %s to recipient %s",
+                        messageId, recipientId);
+                } else {
+                    console.error("Unable to send message.");
+                    console.error(response);
+                    console.error(error);
+                }
+            });
+        }
 
         var users = db.collection("users");
 
